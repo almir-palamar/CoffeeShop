fc9f921e4f24a1b224c0001b7bb783ef2654f984
almirpalamar <almir.palamar@vegait.rs>
2024-06-05 17:14:45 +0200

    deleted unused classes

--- src/main/java/com/example/coffeeshop/exceptions/BadCredentialsException.java
+++
@@ -1,9 +0,0 @@
-package com.example.coffeeshop.exceptions;
-
-import org.springframework.http.HttpStatus;
-
-public class BadCredentialsException extends AppGeneralException{
-    public BadCredentialsException(String message) {
-        super(HttpStatus.UNAUTHORIZED, message);
-    }
-}
