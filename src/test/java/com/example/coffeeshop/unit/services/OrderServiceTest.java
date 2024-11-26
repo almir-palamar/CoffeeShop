bd6ee831a38ebc748f8beffae66d1ee9c238b597
almirpalamar <almir.palamar@vegait.rs>
2024-11-26 14:11:49 +0100

    Write unit tests for order service

---
+++ src/test/java/com/example/coffeeshop/unit/services/OrderServiceTest.java
@@ -0,0 +1,120 @@
+package com.example.coffeeshop.unit.services;
+
+import com.example.coffeeshop.enums.OrderEnum;
+import com.example.coffeeshop.models.Coffee;
+import com.example.coffeeshop.models.Order;
+import com.example.coffeeshop.repositories.OrderRepository;
+import com.example.coffeeshop.services.OrderService;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class OrderServiceTest {
+
+    @InjectMocks
+    private OrderService orderService;
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    @Test
+    void shouldFindOrderById() {
+        List<Coffee> orderCoffees = List.of(
+            Coffee.builder()
+                .id(1L)
+                .name("Espresso")
+                .caffeineGram(8)
+                .brewTime(40)
+                .price(1.60f)
+                .build()
+        );
+
+        Order order = Order.builder()
+                .id(1L)
+                .type(OrderEnum.Type.TO_GO)
+                .status(OrderEnum.Status.PENDING)
+                .coffees(orderCoffees)
+                .build();
+
+        when(orderRepository.findById(order.getId())).thenReturn(Optional.of(order));
+
+        Order foundOrder = orderService.findById(order.getId());
+
+        assertThat(foundOrder.getId()).isEqualTo(order.getId());
+        assertThat(foundOrder.getStatus()).isEqualTo(order.getStatus());
+        assertThat(foundOrder.getType()).isEqualTo(order.getType());
+        assertThat(foundOrder.getCoffees()).isEqualTo(orderCoffees);
+    }
+
+    @Test
+    void shouldUpdateOrderStatus() {
+        List<Coffee> orderCoffees = List.of(
+                Coffee.builder()
+                        .id(1L)
+                        .name("Espresso")
+                        .caffeineGram(8)
+                        .brewTime(40)
+                        .price(1.60f)
+                        .build()
+        );
+        Order order = Order.builder()
+                .id(1L)
+                .type(OrderEnum.Type.TO_GO)
+                .status(OrderEnum.Status.PENDING)
+                .coffees(orderCoffees)
+                .build();
+
+        when(orderRepository.findById(order.getId())).thenReturn(Optional.of(order));
+        when(orderRepository.save(order)).thenReturn(order);
+
+        Order updatedOrder = orderService.updateOrderStatus(order.getId(), OrderEnum.Status.DELIVERED);
+
+        assertThat(updatedOrder.getStatus()).isEqualTo(OrderEnum.Status.DELIVERED);
+    }
+
+    @Test
+    @Disabled
+    void shoudSaveOrder() {
+        //TODO create objectMapper for Order & OrderDTO
+        List<Coffee> orderCoffees = List.of(
+                Coffee.builder()
+                        .id(1L)
+                        .name("Espresso")
+                        .caffeineGram(8)
+                        .brewTime(40)
+                        .price(1.60f)
+                        .build()
+        );
+        Order order = Order.builder()
+                .id(1L)
+                .type(OrderEnum.Type.TO_GO)
+                .status(OrderEnum.Status.PENDING)
+                .coffees(orderCoffees)
+                .build();
+
+        when(orderRepository.save(order)).thenReturn(order);
+
+//        Order newOrder = orderService.save(order);
+
+    }
+
+    @Test
+    void shouldReturnCountOfOrdersByStatusIsAndType() {
+
+        when(orderRepository.countOrderByStatusIsAndType(OrderEnum.Status.PENDING, OrderEnum.Type.TO_GO))
+                .thenReturn(1);
+
+        assertThat(orderService.countOrderByStatusIsAndType(OrderEnum.Status.PENDING, OrderEnum.Type.TO_GO))
+                .isEqualTo(1);
+    }
+}
